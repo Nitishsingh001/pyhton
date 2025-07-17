228b20977f52f2fe7286925292b67e73404b5174
Nitish Singh <nitishsingh92057@gamil.com>
2025-07-17 20:38:09 +0530

    initial and final submission for day4

---
+++ unit_1/sprint_1/day4/index.py
@@ -0,0 +1,36 @@
+
+
+
+# 📌 Day 4: Functions & Modules
+# Topics:
+
+# Defining functions (def)
+
+# Parameters & arguments (positional, keyword, default)
+
+# return vs print
+
+# Lambda functions (lambda x: x*2)
+
+# Modules & imports (import math, from random import randint)
+
+
+
+
+# syntax of function 
+
+
+# def greet(name):
+#     return f"Hello, {name}!"    
+
+# function with no parameters and no return
+
+# def greet():
+#     print("hello world")
+# greet()
+
+# # function with parameters and  return 
+
+# def greet(a,b):
+#     return f"hello, {a +b}"
+# print(greet(1,2))
