df0f50cf75e4229b174a0c8711636549245a2d67
Nitish Singh <nitishsingh92057@gamil.com>
2025-07-17 12:42:07 +0530

    final submission for day1

--- unit_1/sprint_1/day1/index.py
+++ unit_1/sprint_1/day1/index.py
@@ -1,2 +1,39 @@
+name = "nitsh"
+age =21
+is_student = True 
 
-print("hellow")
+name = input("Enter your name:")
+print("hello", name)
+
+
+print("Welcome to the Python course!")  
+
+
+age = int(input("enter your age: "))
+
+print("your age is ", age)
+
+# practice time 
+
+
+# take name and age as input and print a greeting message
+
+name = input("Enter your name; ")
+age  = int(input("Entet your age: "))
+print("The name is ", name , "and the age is ", age)
+
+
+firstnum = int(input("Enter first numer : "))
+secondnum = int(input("Enter second number: "))
+sum = firstnum +secondnum
+print("The is ", sum)
+
+# formula (c*9/5) + 32
+cel = float(input("Enter temperature in Celsius:"))
+far = (cel * 9/5) + 32
+print("The temperature in Farenheit is : " , far)
+
+
+
+
+
