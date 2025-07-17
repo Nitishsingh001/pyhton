541514708f686c4a2579eff89682618194d4cfd2
Nitish Singh <nitishsingh92057@gamil.com>
2025-07-17 21:50:56 +0530

    initial submission of day 5

---
+++ unit_1/sprint_1/day5/index.py
@@ -0,0 +1,10 @@
+# 📌 Day 5: File Handling & Exception Handling
+# Topics:
+
+# Reading/writing files (open(), read(), write())
+
+# with statement (context manager)
+
+# Exception handling (try, except, finally)
+
+# Raising exceptions (raise)
