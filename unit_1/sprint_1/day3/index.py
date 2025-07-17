79db35d425880d2228880ce07f2be93adda991ea
Nitish Singh <nitishsingh92057@gamil.com>
2025-07-17 18:24:26 +0530

    initial submission of day3

---
+++ unit_1/sprint_1/day3/index.py
@@ -0,0 +1,14 @@
+
+
+#  Day 3: Data Structures (Lists, Tuples, Sets, Dictionaries)
+# Topics:
+
+# Lists (indexing, slicing, methods like append(), remove())
+
+# Tuples (immutable, unpacking)
+
+# Sets (union, intersection, difference)
+
+# Dictionaries (keys(), values(), items())
+
+# List/Dictionary comprehensions
