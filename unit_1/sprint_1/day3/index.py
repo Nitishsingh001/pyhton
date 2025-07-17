b21965f4dd273ede063f044b6b3ba74bfb780c82
Nitish Singh <nitishsingh92057@gamil.com>
2025-07-17 19:20:11 +0530

    list tuples dictonaryF

--- unit_1/sprint_1/day3/index.py
+++ unit_1/sprint_1/day3/index.py
@@ -12,3 +12,48 @@
 # Dictionaries (keys(), values(), items())
 
 # List/Dictionary comprehensions
+
+
+
+
+#  1.> Lists (indexing, slicing, methods like append(), remove())
+
+
+# Example of a list
+
+# fruits = ["apple", "banana", "cherry","mango","grape"]
+# # fruits.append("orange")
+# # fruits.insert(1, "kiwi")
+# # fruits.sort()
+
+# print(fruits::2)
+
+
+# inp = int(input("Enter your number  "))
+# list1 = []
+# for i in range(1,5,1 ):
+#     list1.append(inp)
+
+# print("The list is ", list1)
+
+# tuples (immutable, unpacking)
+
+# my_tuple = 1,2,3,4,5
+
+# print(my_tuple[1])
+
+# person = ("John", 25, "Engineer")
+# name, age, job = person
+
+# print(name)  # Output: John
+# print(age)   # Output: 25
+# print(job)   # Output: Engineer
+
+
+# # Sets (union, intersection, difference)
+
+# my_set = {1, 2, 3, 4}
+# my_set.add(5)
+# my_set.remove(2)    
+
+# print(my_set)
