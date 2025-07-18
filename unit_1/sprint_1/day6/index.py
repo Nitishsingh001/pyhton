49f4bde7dc9ffa2cd8d60be15a822f74cc35235e
Nitish Singh <nitishsingh92057@gamil.com>
2025-07-18 15:46:08 +0530

    inital submission

---
+++ unit_1/sprint_1/day6/index.py
@@ -0,0 +1,29 @@
+# 📌 Day 6: Object-Oriented Programming (OOP)
+# Topics:
+
+# Classes & objects (class, self)
+
+# Constructors (__init__)
+
+# Inheritance (class Child(Parent))
+
+# Encapsulation (private/public attributes)
+
+# Polymorphism & Method overriding
+
+
+# class Animal:
+#     def add(self,a,b):
+#         return a+b
+    
+# s1 = Animal()
+# p1 = s1.add(10,20)
+# print("sum :",p1)
+
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def display(self):
+#         print(f"My name is {self.name} and I am {self.age} years old.")
