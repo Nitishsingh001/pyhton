1ed3f304dcb238be6cbf0288620e8d1bcf6cbda1
Nitish Singh <nitishsingh92057@gamil.com>
2025-07-18 16:34:41 +0530

    initial submission for day7

---
+++ unit_1/sprint_1/day7/index.py
@@ -0,0 +1,44 @@
+
+# 📌 Day 7: Advanced OOP & Magic Methods
+# Topics:
+
+# Class methods (@classmethod) & Static methods (@staticmethod)
+
+# Magic methods (__str__, __repr__, __add__)
+
+# Abstract Base Classes (ABC, @abstractmethod)
+
+# Property decorators (@property, @x.setter)
+
+
+# class MathTools:
+    
+#     @staticmethod
+#     def add(x, y):
+#         return x + y
+
+#     @staticmethod
+#     def multiply(x, y):
+#         return x * y
+
+# # Call without creating object
+# print(MathTools.add(10, 5))       # 15
+# print(MathTools.multiply(4, 3))   # 12
+
+
+
+# class MathTools:
+    
+#     @staticmethod
+#     def add(x, y):
+#         return x + y
+
+#     @staticmethod
+#     def multiply(x, y):
+#         return x * y
+
+# # Call without creating object
+# print(MathTools.add(10, 5))       # 15
+# print(MathTools.multiply(4, 3))   # 12
+
+
