27ca692d0544fcdb6b3432c232428a95693631a7
Nitish Singh <nitishsingh92057@gamil.com>
2025-07-19 11:19:24 +0530

    initial submission

---
+++ unit_1/sprint_1/day9/index.py
@@ -0,0 +1,10 @@
+# 📌 Day 9: Functional Programming & Decorators
+# Topics:
+
+# map(), filter(), reduce()
+
+# Closures & Decorators (@decorator)
+
+# Generators (yield keyword)
+
+# *args & **kwargs
