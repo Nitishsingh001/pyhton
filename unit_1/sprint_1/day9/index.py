13f1e5a0dae7810a05ff2f32401f94047af7dff4
Nitish Singh <nitishsingh92057@gamil.com>
2025-07-19 11:55:37 +0530

    intermediate submission

--- unit_1/sprint_1/day9/index.py
+++ unit_1/sprint_1/day9/index.py
@@ -8,3 +8,43 @@
 # Generators (yield keyword)
 
 # *args & **kwargs
+
+
+
+# map()
+
+# using two parameters one is function and other is iterable 
+
+
+# numbers= [1,2,3,4,5]
+
+
+# def square(x):
+#     return x*x
+
+# squared_numbers = list(map(square, numbers))
+# print(squared_numbers)
+
+# # using lambda function 
+
+# squared_numbers_lambda = list(map(lambda x: x*x, numbers))
+# print(squared_numbers_lambda)
+
+# numbers = [1, 2, 3, 4, 5, 6]
+
+# def is_even(x):
+#     return x % 2 == 0
+
+# evens = list(filter(is_even, numbers))
+# print(evens)
+
+
+from functools import reduce
+
+numbers = [1, 2, 3, 4]
+
+def multiply(x, y):
+    return x * y
+
+result = reduce(multiply, numbers)
+print(result)
